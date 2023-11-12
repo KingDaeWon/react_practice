056691b9b871d2b0c29c3e23a59d9757eb70ee09
김대원 <115925564+KingDaeWon@users.noreply.github.com>
2023-11-13 05:20:41 +0900

    리액트 연습

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = document.querySelector("#root");
+
+const app = (
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
+
+const rootElement = createRoot(root);
+rootElement.render(app);
